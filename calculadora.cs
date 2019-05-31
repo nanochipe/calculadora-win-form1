4f5fc84935959a5ee8f04b7405a47045b4938e06
nanochipe <nanochipe@gmail.com>
2019-05-30 21:59:50 -0300

    deu certo

---
+++ calculadora.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Double valor1, valor2, res;
+            valor1 = double.Parse(textvalor1.Text);
+            valor2 = double.Parse(textvalor2.Text);
+            res = valor1 + valor2;
+            textBox3.Text = res.ToString();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
